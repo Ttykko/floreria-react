f15d3167c169d551ff2a357c0eda906bd023d27e
Alberto Roblero <alberto.rcny@gmail.com>
2026-09-17 16:59:14 -0300

    chore: sanitizar recursos visuales, nombres y contacto para modo demo

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,12 +4,12 @@ import "./Footer.scss";
 
 function Footer() {
   // Enlaces de Google Maps reales con la dirección exacta para Quillota y La Cruz
-  const urlLocalPrincipal = "https://maps.app.goo.gl/tD4qLEsXKVBCJ86NA";
-  const urlLocalLaCruz = "https://maps.app.goo.gl/4S5CgS8a9eb1Geiq8";
+  const urlLocalPrincipal = "https://www.google.com/maps/place/Vi%C3%B1a+del+Mar,+Valpara%C3%ADso/@-33.0053579,-71.6126582,12z/data=!3m1!4b1!4m6!3m5!1s0x9689de84ead41255:0x8e5fde76df3d413f!8m2!3d-33.0153481!4d-71.5500276!16zL20vMDFybnR5?entry=ttu&g_ep=EgoyMDI2MDkxNC4wIKXMDSoASAFQAw%3D%3D";
+  const urlLocalLaCruz = "https://www.google.com/maps/place/Bosque+Natura/@-33.2409119,-71.6644145,16z/data=!4m15!1m8!3m7!1s0x9689de84ead41255:0x8e5fde76df3d413f!2zVmnDsWEgZGVsIE1hciwgVmFscGFyYcOtc28!3b1!8m2!3d-33.0153481!4d-71.5500276!16zL20vMDFybnR5!3m5!1s0x96620d2e1174417b:0xef0c401508de5559!8m2!3d-33.243319!4d-71.658265!16s%2Fg%2F11g2tl6qby?entry=ttu&g_ep=EgoyMDI2MDkxNC4wIKXMDSoASAFQAw%3D%3D";
 
   // Enlaces de redes sociales oficiales de la florería
-  const urlInstagram = "https://www.instagram.com/jardind.amore/";
-  const urlThreads = "https://www.threads.com/@jardind.amore?xmt=AQG0i9-FV71gyTm2_DynYFgstrEi7Bl3-RSSb8tysHjSAN8"; 
+  const urlInstagram = "https://www.instagram.com/?hl=pt-br";
+  const urlThreads = "https://www.threads.com/?xmt=AQG0i9-FV71gyTm2_DynYFgstrEi7Bl3-RSSb8tysHjSAN8"; 
 
   return (
     <footer className="main-footer">
@@ -80,8 +80,8 @@ function Footer() {
                 <span className="ping"></span> {/* Onda expansiva del radar */}
               </div>
               <div className="location-text">
-                <span className="loc-title">Local Principal (101)</span>
-                <span className="loc-desc">Av. Carlos Condell 101, Quillota</span>
+                <span className="loc-title">Local Principal </span>
+                <span className="loc-desc">Viña del Mar</span>
               </div>
             </a>
 
@@ -96,8 +96,8 @@ function Footer() {
                 <FaCompass />
               </div>
               <div className="location-text">
-                <span className="loc-title">Sucursal La Cruz</span>
-                <span className="loc-desc">Paradero 13, La Cruz</span>
+                <span className="loc-title">Sucursal 2</span>
+                <span className="loc-desc">Paradero 11</span>
               </div>
             </a>
 
